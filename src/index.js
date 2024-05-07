b2813590da5d0f44cc32018a4c5e0cafc5e2c8c7
Matheus Keske <keskem6@gmail.com>
2024-05-07 11:37:26 -0300

    commit inicial

---
+++ src/index.js
@@ -0,0 +1,12 @@
+"use strict";
+// Função de multiplicação
+function multiplicacao(base, altura) {
+    return base * altura;
+}
+// Função de saudação
+function saudacao(nome) {
+    return "Olá " + nome;
+}
+// Exemplos de uso das funções
+console.log(multiplicacao(4, 6)); // Saída: 6
+console.log(saudacao("João")); // Saída: Olá João
---
+++ src/index.ts
@@ -0,0 +1,13 @@
+// Função de multiplicação
+function multiplicacao(base: number, altura: number): number {
+    return base * altura;
+}
+
+// Função de saudação
+function saudacao(nome: string): string {
+    return "Olá " + nome;
+}
+
+// Exemplos de uso das funções
+console.log(multiplicacao(4, 6)); // Saída: 6
+console.log(saudacao("João")); // Saída: Olá João
